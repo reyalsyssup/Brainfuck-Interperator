be790bc9dbfc674b1a5a8a72bfd05da85e7672f3
reyalsyssup <63643459+reyalsyssup@users.noreply.github.com>
2020-09-03 17:39:48 +1000

    loops work

--- interperator.cpp
+++ interperator.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
+#include <vector>
 
 void compile(std::string data) {
     int tape[30000] = {0};
     int ptr = 0;
+    std::vector<int> loopIndexes = std::vector<int>();
+    int loops = 0;
+    bool isLoop = false;
 
     for(int i = 0; i < data.length(); i++) {
+        if(isLoop) {
+            if(data[i] == '[') loops++;
+            if(data[i] == ']') {
+                // if there are no loops happening
+                if(tape[ptr] == 0) isLoop = false;
+                else loops--;
+            }
+        }
+
         if(data[i] == '+') tape[ptr]++;
         else if(data[i] == '-') tape[ptr]--;
         else if(data[i] == '>') ptr++;
@@ -15,11 +28,21 @@ void compile(std::string data) {
             std::cin >> input;
             tape[ptr] = int(input);
         }
+        else if(data[i] == '[') {
+            if(tape[ptr] == 0) isLoop = true;
+            else loopIndexes.push_back(i);
+        }
+        else if(data[i] == ']') {
+            if(tape[ptr] != 0) i = loopIndexes[loopIndexes.size()-1];
+            else loopIndexes.pop_back();
+        }
     }
-    for(int i = 0; i < 20; i++) {
-        std::cout << tape[i] << " ";
-    }
-    std::cout << "\n";
+    // this prints out the tape
+    // only used in dev or debugging
+    // for(int i = 0; i < 20; i++) {
+    //     std::cout << tape[i] << " ";
+    // }
+    // std::cout << "\n";
 }
 
 int main() {
