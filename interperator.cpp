dc94086d03d0598123ccf54fcefa83e668a49a5c
reyalsyssup <63643459+reyalsyssup@users.noreply.github.com>
2020-09-03 17:05:59 +1000

    6/8 commands done. Need to do loops

--- interperator.cpp
+++ interperator.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
 
+void compile(std::string data) {
+    int tape[30000] = {0};
+    int ptr = 0;
+
+    for(int i = 0; i < data.length(); i++) {
+        if(data[i] == '+') tape[ptr]++;
+        else if(data[i] == '-') tape[ptr]--;
+        else if(data[i] == '>') ptr++;
+        else if(data[i] == '<') ptr--;
+        else if(data[i] == '.') std::cout << char(tape[ptr]);
+        else if(data[i] == ',') {
+            char input;
+            std::cin >> input;
+            tape[ptr] = int(input);
+        }
+    }
+    for(int i = 0; i < 20; i++) {
+        std::cout << tape[i] << " ";
+    }
+    std::cout << "\n";
+}
+
 int main() {
-    
+    std::string code;
+    std::cin >> code;
+    compile(code);
 }
